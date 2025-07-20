c94be91b4b76cd7d8896db81f26a8adca081c5ef
ShuhaibNC <shuhaibsaad5@gmail.com>
2025-07-21 02:37:13 +0530

    for koyeb

--- start.py
+++ start.py
@@ -1,10 +1,25 @@
 from pyrogram import Client
 import config
 import logging
+import asyncio
+from aiohttp import web
+from route import routes
 
 logging.basicConfig(level=logging.INFO)
 
 plugins = dict(root='plugins')
+
+# Web server for route integration
+async def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
+    
+loop = asyncio.get_event_loop()
+web_app = loop.run_until_complete(web_server())
+web_runner = web.AppRunner(web_app)
+loop.run_until_complete(web_runner.setup())
+loop.create_task(web.TCPSite(web_runner, "0.0.0.0", 8080).start())
 
 Client(
     'NightAIBot',
